5b47fe1e0a9efdd8546011a73699c16f4c1aae2a
Nikko D <ndasalla1323@gmail.com>
2023-06-27 20:47:37 -0400

    implemented searchbar, todo: style searchbar and finish modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 // import Recipes from "./components/Recipes";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeCard from "./components/RecipeCard";
+import SearchBar from "./components/SearchBar";
 import Modal from "./ui/Modal";
 import recipeData from "./utils/recipeData";
 
 const App = () => {
   const [recipes, setRecipes] = useState(recipeData);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [search, setSearch] = useState("");
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -31,15 +33,24 @@ const App = () => {
     <RecipeCard recipe={recipe} key={i} />
   ));
 
+  useEffect(() => {
+    setRecipes(
+      recipeData.filter((recipe) => {
+        return recipe.name.toUpperCase().includes(search.toUpperCase());
+      })
+    );
+  }, [search]);
+
   /*TODOS
     - navbar, margin/padding
   */
   return (
     <div className="max-w-7xl mx-auto text-center">
-      <p className="text-7xl">Recipe? Recipe!</p>
+      <p className="text-7xl pb-8">Recipe? Recipe!</p>
+      <SearchBar search={search} setSearch={setSearch} />
       <div>
         <button
-          className="bg-blue-500 px-4 py-2 hover:bg-blue-600 transition"
+          className="bg-blue-500 px-4 py-2 my-6 hover:bg-blue-600 transition"
           onClick={showModal}
         >
           + Add New Recipe
